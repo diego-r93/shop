bcec1c365486cfae2796ccb1e3f8f994f46602ab
diego <diegorodrigues-5@poli.ufrj.br>
2022-02-10 07:17:46 -0300

    :tada: Initial commit

---
+++ lib/models/product_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+import '../data/dummy_data.dart';
+
+class ProductList with ChangeNotifier {
+  final List<Product> _items = dummyProducts;
+
+  List<Product> get items => [..._items];
+  List<Product> get favoriteItems =>
+      _items.where((prod) => prod.isFavorite).toList();
+
+  void addProduct(Product product) {
+    _items.add(product);
+    notifyListeners();
+  }
+}
+
+// bool _showFavoriteOnly = false;
+
+//   List<Product> get items {
+//     if (_showFavoriteOnly) {
+//       return _items.where((prod) => prod.isFavorite).toList();
+//     }
+
+//     return [..._items];
+//   }
+
+//   void showFavoriteOnly() {
+//     _showFavoriteOnly = true;
+//     notifyListeners();
+//   }
+
+//   void showAll() {
+//     _showFavoriteOnly = false;
+//     notifyListeners();
+//   }
+
+//   void addProduct(Product product) {
+//     _items.add(product);
+//     notifyListeners();
+//   }
